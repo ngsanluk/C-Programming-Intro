8c03d6df19643486eaca1a2fab8231c26ca8c5c3
Sunny NG <ngsanluk@gmail.com>
2021-07-02 11:25:49 +0800

    update week 7 pointer example

--- W07/pointer.c
+++ W07/pointer.c
@@ -2,10 +2,39 @@
 
 int main( ) {
 
+  int salary = 1000;
+  int salary2 = 5000;
   int * n;
-  *n = 100;
-  printf("n = %d\n", * n);
-  printf("*n =  %p (memory address)\n", n);
+  int ** m;
+  n = &salary;
+  printf("&salary =  %p (memory address)\n", &salary);
+  printf("n =  %p (memory address)\n", n);
+  printf("*n = %d\n", * n);
+
+  *n = 2000;
+  printf("\n\nn =  %p (memory address)\n", n);
+  printf("&n =  %p (memory address)\n", &n);
+  printf("*n = %d\n", * n);
+  printf("salary = %d\n", salary);
+  printf("salary2 = %d\n", salary2);
+
+  n = &salary2;
+  *n = 50000;
+  printf("\n\nn =  %p (memory address)\n", n);
+  printf("&n =  %p (memory address)\n", &n);
+  printf("*n = %d\n", * n);
+  printf("salary = %d\n", salary);
+  printf("salary2 = %d\n", salary2);
+
+
+  m = &n;
+  *m = &salary;
+  *n = 3000;
+  printf("\n\nn =  %p (memory address)\n", n);
+  printf("&n =  %p (memory address)\n", &n);
+  printf("*n = %d\n", * n);
+  printf("salary = %d\n", salary);
+  printf("salary2 = %d\n", salary2);
 
   return 0;
 }
